753e3b8d9a3c46867115e1c81691765388531ef7
shanni <shanice598@yahoo.co.uk>
2025-06-05 09:29:42 +0100

    adding controller for comments

--- src/main/java/com/makersacademy/acebook/controller/CommentController.java
+++ src/main/java/com/makersacademy/acebook/controller/CommentController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/comments")
+@RequestMapping("/post/comments")
 public class CommentController {
 
     private final CommentService commentService;
